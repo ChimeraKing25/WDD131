954ccbbc40195565358b6c2063c8e65b10b8138e
Alexander Palacios <alexp02@byui.edu>
2025-05-04 17:26:20 -0600

    take 3

--- mission/mission.js
+++ mission/mission.js
@@ -1,17 +1,18 @@
 const themeSelector = document.querySelector('#theme');
-const body = document.body;
+const container = document.querySelector('.container'); 
 const logo = document.querySelector('#logo');
 
 function changeTheme() {
   const theme = themeSelector.value;
 
   if (theme === 'dark') {
-    body.classList.add('dark');
+    container.classList.add('dark'); 
     logo.src = 'byui-logo_white.png';
   } else {
-    body.classList.remove('dark');
+    container.classList.remove('dark');
     logo.src = 'byui-logo_blue.webp';
   }
 }
 
 themeSelector.addEventListener('change', changeTheme);
+
