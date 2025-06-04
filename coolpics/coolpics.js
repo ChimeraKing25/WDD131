832ec4945b3e078c230e010ac745ce18f6f62f95
Alexander Palacios <alexp02@byui.edu>
2025-06-04 12:32:44 -0600

    Coolpics

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,5 +1,44 @@
-document.getElementById("menu").addEventListener("click", () => {
-    const nav = document.getElementById("main-nav");
-    nav.classList.toggle("open");
-  });
-  
+
+
+menuButton.addEventListener("click", toggleMenu);
+function handleResize() {
+  if (window.innerWidth > 1000) {
+    menu.classList.remove("hide");
+  } else {
+    menu.classList.add("hide");
+  }
+}
+
+handleResize();
+window.addEventListener("resize", handleResize);
+
+const gallery = document.querySelector(".gallery"); // Your <div class="gallery"> should contain the thumbnails
+const modal = document.querySelector(".image-modal");
+
+function openModal(event) {
+  const clickedImg = event.target.closest("img");
+  if (!clickedImg) return;
+
+  const src = clickedImg.getAttribute("src");
+  const alt = clickedImg.getAttribute("alt") || "";
+
+  const fullSrc = src.split("-")[0] + "-full.jpeg";
+
+   modal.innerHTML = `
+    <img src="${fullSrc}" alt="${alt}">
+    <button class="close-viewer">X</button>
+  `;
+
+   modal.showModal();
+
+    const closeButton = modal.querySelector(".close-viewer");
+  closeButton.addEventListener("click", () => modal.close());
+}
+
+modal.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.close();
+  }
+});
+
+gallery.addEventListener("click", openModal);
